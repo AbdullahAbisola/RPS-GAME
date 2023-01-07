0e2f9c1b3d8ba495baa623277a23536246ed0eb1
abdullah_abisola <abdullahabisola@gmail.com>
2023-01-08 00:40:32 +0200

    Realized that a few classes were redundant

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <div className="game__bg">
+      <div>
         <Dashboard />
         <GameFace />
         <Rules />
